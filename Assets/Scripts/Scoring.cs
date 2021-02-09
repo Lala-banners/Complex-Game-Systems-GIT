35a02b02cfa022042fc22ac72fb338dbee80d8b1
Lala-banners <lala.l.banno@hotmail.com>
2021-02-09 14:54:21 +1100

    Ragdoll physics and scoring script

---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Scoring : MonoBehaviour
+{
+    public TMP_Text scoreText;
+
+    List<Joint> joints; //List of joints in the character
+
+    [SerializeField]
+    private float minForceForScore = 1000; //Minimum amount of force needed to get a score
+
+    [SerializeField]
+    private float scoreMultiplier;
+
+    public float currentScore = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joints = new List<Joint>(GetComponentsInChildren<Joint>());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float appliedForce = 0f;
+
+        foreach (Joint joint in joints)
+        {
+            appliedForce += joint.currentForce.magnitude;
+        }
+
+        if (appliedForce > minForceForScore) //If applied force is greater than the min force allowed for scoring
+        {
+            currentScore += (appliedForce * scoreMultiplier * Time.deltaTime); //Current score plus/equal to applied force
+            scoreText.text = "Score: " + (int)currentScore; //Update the score text
+        }
+
+        Debug.Log(appliedForce);
+    }
+}
